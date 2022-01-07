9728da641a66c0bb9bd739128120a6b90dc6bd27
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-01-07 10:48:41 +0700

    Setup Template

---
+++ public/js/app.js
@@ -0,0 +1,16 @@
+import "../scss/app.scss";
+
+// AdminKit (required)
+import "./modules/bootstrap.mjs";
+import "./modules/sidebar.mjs";
+import "./modules/theme.mjs";
+import "./modules/feather.mjs";
+
+// Charts
+import "./modules/chartjs.mjs";
+
+// Forms
+import "./modules/flatpickr.mjs";
+
+// Maps
+import "./modules/vector-maps.mjs";
