555affee8bd4a340c7c6b47fdc1da94e11ded6d2
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-01-13 21:54:14 +0700

    Add method antoher method override

--- app.js
+++ app.js
@@ -44,10 +44,10 @@ app.use(express.json());
 app.use(cors());
 
 // Method override
+app.use(methodOverride("_method"));
 app.use(
   methodOverride(function (req, res) {
     if (req.body && typeof req.body === "object" && "_method" in req.body) {
-      // look in urlencoded POST bodies and delete it
       let method = req.body._method;
       delete req.body._method;
       return method;
