94ccff3ebd49853ce2b786492825d0dd98f15228
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-01-16 19:20:34 +0700

    remove progress field from project model and init get project tasks

--- src/models/Project.model.js
+++ src/models/Project.model.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
-import { PROJECT_STATUS_ENUM } from "../utils/constants.js";
+import { PROJECT_STATUS_ENUM, TASK_STATUS_ENUM } from "../utils/constants.js";
+import TaskModel from "./Task.Model.js";
 
 const projectSchema = mongoose.Schema(
   {
@@ -19,9 +20,6 @@ const projectSchema = mongoose.Schema(
       enum: Object.values(PROJECT_STATUS_ENUM),
       default: PROJECT_STATUS_ENUM.TODO,
     },
-    progress: {
-      type: Number,
-    },
     isSelected: {
       type: Boolean,
       default: false,
@@ -40,6 +38,23 @@ const projectSchema = mongoose.Schema(
     timestamps: true,
   }
 );
+
+projectSchema.methods.getTasks = async function () {
+  const tasks = await TaskModel.find({ project: this._id });
+  const progress = tasks.reduce(
+    (c, { status }) => {
+      if (status == TASK_STATUS_ENUM.TODO) c.todo++;
+      if (status == TASK_STATUS_ENUM.DONE) c.done++;
+      if (status == TASK_STATUS_ENUM.PROGRESS) c.inProggress++;
+      c.total++;
+      c.percentage = (c.done / c.total) * 100;
+      return c;
+    },
+    { todo: 0, inProggress: 0, done: 0, total: 0, percentage: 0 }
+  );
+
+  return { tasks, progress };
+};
 
 const ProjectModel = mongoose.model("ProjectModel", projectSchema, "projects");
 
