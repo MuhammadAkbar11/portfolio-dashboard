f04e8fb948644844ce3dece92c17f7e6823ad25b
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-01-17 20:55:01 +0700

    Edit Project task

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -1,5 +1,5 @@
 import { validationResult } from "express-validator";
-import { ValidationError } from "../helpers/baseError.helper.js";
+import BaseError, { ValidationError } from "../helpers/baseError.helper.js";
 import TaskModel from "../models/Task.Model.js";
 
 export const postProjectTask = async (req, res) => {
@@ -26,6 +26,51 @@ export const postProjectTask = async (req, res) => {
     req.flash("flashdata", {
       type: "success",
       message: "Success add a task",
+    });
+    res.redirect(successRedirect);
+  } catch (error) {
+    req.flash("flashdata", {
+      type: "danger",
+      message: "Failed to create task",
+    });
+    res.redirect(`${errorRedirect}?task_action_error=true`);
+  }
+};
+
+export const putProjectTask = async (req, res) => {
+  const id = req.params.id;
+  const { note, projectId, status, _render } = req.body;
+  const successRedirect = req.query.successRedirect || "/projects/" + projectId;
+  const errorRedirect = req.query.errorRedirect || "/projects/" + projectId;
+
+  try {
+    const validate = validationResult(req);
+
+    if (!validate.isEmpty()) {
+      const errValidate = new ValidationError(validate.array(), _render, {
+        values: req.body,
+        taskActionError: true,
+      });
+
+      req.flash("errors", errValidate);
+      res.redirect(`${errorRedirect}?task_action_error=true`);
+      return;
+    }
+
+    const task = await TaskModel.findById(id);
+
+    if (!task) {
+      throw new BaseError("Not Found", 400, "Task Not Found", true, {});
+    }
+
+    task.note = note;
+    task.status = status;
+
+    await task.save();
+
+    req.flash("flashdata", {
+      type: "success",
+      message: "Success edit task",
     });
     res.redirect(successRedirect);
   } catch (error) {
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,9 +1,14 @@
-import { postProjectTask } from "../controllers/task.controller.js";
+import {
+  postProjectTask,
+  putProjectTask,
+} from "../controllers/task.controller.js";
 import { taskValidate } from "../helpers/validation.helper.js";
-
 import { ensureAuth } from "../middleware/auth.js";
 
 function TaskRoutes(app) {
+  app
+    .route("/tasks/:id")
+    .put(ensureAuth, taskValidate("create"), putProjectTask);
   app.route("/tasks").post(ensureAuth, taskValidate("create"), postProjectTask);
 }
 
