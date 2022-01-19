4d40206022392d203b10c0cbdf00d7e8d64fbabf
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-01-19 23:23:46 +0700

    clean up project modals file component

--- public/js/components/project/projectModals.mjs
+++ public/js/components/project/projectModals.mjs
@@ -1,13 +1,12 @@
+import { createProjectTaskEditModal } from "../tasks/taskModalAction.mjs";
+
 const ProjectDeleteModal = document.getElementById("modalDeleteProject");
 
 const initializeProjectModals = () => {
   initModalDelete();
-  initProjectTaskActionModal();
 };
 
 const initModalDelete = () => {
-  // if(ProjectModalDelete)
-
   const ToggleProjectDeleteModal = [].slice.call(
     document.querySelectorAll("#action-delete-project")
   );
@@ -37,9 +36,6 @@ const handleShowDeleteProjectModal = data => {
     <p>Are your sure want to delete this <span class="text-danger fw-bold" >${data.title}</span> project? <br> you can't undo this action </p>
     `;
 
-    // const ModalDeleteConfirm = ProjectDeleteModal.querySelector(".action-confirm");
-    // const FormDelete = document.createElement("form");
-
     const modalEl = new bootstrap.Modal(
       document.getElementById("modalDeleteProject"),
       {
@@ -53,96 +49,6 @@ const handleShowDeleteProjectModal = data => {
       console.log("shown");
     });
   }
-};
-
-const initProjectTaskActionModal = () => {
-  const FormProjectTaskModal = document.getElementById("formTaskModal");
-  const ProjectEditTaskActions = [].slice.call(
-    document.querySelectorAll("#task-edit")
-  );
-
-  if (FormProjectTaskModal) {
-    const isErr = FormProjectTaskModal.dataset.error;
-    if (isErr == "true") {
-      FormProjectTaskModal.classList.remove("fade");
-      const modalEl = new bootstrap.Modal(
-        document.getElementById("formTaskModal"),
-        {
-          keyboard: false,
-        }
-      );
-      modalEl.show();
-    }
-
-    FormProjectTaskModal.addEventListener("hidden.bs.modal", function (event) {
-      ResetProjectTaskEditModal(event.target);
-    });
-  }
-
-  ProjectEditTaskActions.forEach(x => {
-    x.setAttribute("href", "javascript:void(0)");
-
-    x.addEventListener("click", e => {
-      e.preventDefault();
-      const target = e.target;
-      const dataTask = JSON.parse(e.target.dataset.task);
-      CreateProjectTaskEditModal(dataTask);
-    });
-  });
-};
-
-const CreateProjectTaskEditModal = task => {
-  const FormProjectTaskModal = document.getElementById("formTaskModal");
-  const ModalTitle = FormProjectTaskModal.querySelector(".modal-title");
-  const ModalBody = FormProjectTaskModal.querySelector(".modal-body");
-  const FormModal = ModalBody.querySelector("#project-task-form");
-
-  FormModal.setAttribute("action", "/tasks/" + task._id);
-  FormModal.querySelector("[name=_method]").value = "PUT";
-  FormModal.querySelector("[name=note]").value = task.note;
-  const TaskStatusInput = FormModal.querySelector("[name=status]");
-
-  [].slice.call(TaskStatusInput.querySelectorAll("option")).map(op => {
-    op.removeAttribute("selected");
-    if (op.value == task.status) op.setAttribute("selected", true);
-  });
-
-  ModalTitle.innerText = "Edit Task";
-  FormProjectTaskModal.classList.add("fade");
-  const modalEl = new bootstrap.Modal(
-    document.getElementById("formTaskModal"),
-    {
-      keyboard: false,
-      backdrop: true,
-    }
-  );
-  modalEl.show();
-};
-
-const ResetProjectTaskEditModal = modalEl => {
-  const ModalTitle = modalEl.querySelector(".modal-title");
-  const ModalBody = modalEl.querySelector(".modal-body");
-  const FormModal = ModalBody.querySelector("#project-task-form");
-
-  FormModal.setAttribute("action", "/tasks/");
-  FormModal.querySelector("[name=_method]").value = "POST";
-  FormModal.querySelector("[name=note]").value = "";
-  const TaskStatusInput = FormModal.querySelector("[name=status]");
-
-  [].slice.call(TaskStatusInput.querySelectorAll("option")).map((op, i) => {
-    op.removeAttribute("selected");
-    if (i == 0) op.setAttribute("selected", true);
-  });
-
-  ModalTitle.innerText = "Create Task";
-  modalEl.classList.add("fade");
-  //   const modalEl = new bootstrap.Modal(
-  //     document.getElementById("formTaskModal"),
-  //     {
-  //       keyboard: false,
-  //     }
-  //   );
-  //   modalEl.show();
 };
 
 document.addEventListener("DOMContentLoaded", () => initializeProjectModals());
