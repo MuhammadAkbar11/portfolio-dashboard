8f94b26faa2f4832230f84af9813ea84c39108ac
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-01-20 12:26:28 +0700

    Socket delete project task endpoint

--- src/socker/handlers/task.socker.js
+++ src/socker/handlers/task.socker.js
@@ -1,3 +1,4 @@
+import BaseError from "../../helpers/baseError.helper.js";
 import ProjectModel from "../../models/Project.model.js";
 import TaskModel from "../../models/Task.Model.js";
 
@@ -39,6 +40,32 @@ function RegisterTaskHandlers(io, socket) {
     } catch (error) {
       console.log(error);
       cb && cb(error, null);
+    }
+  });
+
+  socket.on("delete-project-task", async ({ projectId, taskId }, cb) => {
+    try {
+      const task = await TaskModel.findById(taskId);
+      const taskStatus = task.status;
+
+      if (!task) {
+        throw new BaseError("Not Found", 400, "Task Not Found", true, {
+          responseType: "json",
+        });
+      }
+      await task.remove();
+
+      const totalTaskByStatus = await TaskModel.find({
+        project: projectId,
+        status: taskStatus,
+      }).countDocuments();
+
+      console.log(totalTaskByStatus);
+
+      cb(null, { message: "Delete Task" });
+    } catch (error) {
+      console.log(error);
+      cb && cb(error);
     }
   });
 }
